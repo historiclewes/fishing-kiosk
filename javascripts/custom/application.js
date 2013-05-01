7f9cac6ee62343267afb5e2de4c067077378c11c
nickBumgarner <nick@inclind.com>
2013-05-01 10:54:22 -0400

    Finished The Bay template, uploading template files and directorys for other pages.

--- javascripts/custom/application.js
+++ javascripts/custom/application.js
@@ -1,7 +1,18 @@
 var Kiosk = (function($, window, document, undefined) {
   $(document).ready(function() {
     Kiosk.go();
+
+//    $('nav#footer-navigation a').bind(click, function(){
+//
+//      var id = $(this).attr('id');
+//      $(this).addClass('active');
+//      console.log("Hello");
+//    });
+
+
   });
+
+
 
   var jsonFeeds = {
     proggit: 'http://www.reddit.com/r/programming/.json',
@@ -24,7 +35,7 @@ var Kiosk = (function($, window, document, undefined) {
         var source = $("#set-title").html();
         var template = Handlebars.compile(source);
         var context = {
-          title: 'Historic Lewes Kiosk'
+          title: 'Pilots on the Bay & River Delaware'
         };
 
         $("div#page-title").append(template(context));
@@ -32,57 +43,53 @@ var Kiosk = (function($, window, document, undefined) {
       },
 
       pageContent: function() {
-        var source = $("#set-content").html();
-        var template = Handlebars.compile(source);
-        var context = {
-          content: 'Page content is being displayed dynamically from javascript templates.'
-        };
+       Kiosk.page('templates/home/index.html');
 
-        $("div#main-content").append(template(context));
-      },
-
-      generateBuoys: function() {
-        Zepto.ajax({
-          url: './templates/buoys/index.html',
-          type: 'GET',
-          cache: true,
-          success: function (data) {
-            template = Handlebars.compile(data);
-            var context = {
-              title: 'Weather Buoys',
-              buoys: [
-                { href : "#", id : "BUOY1", name : "Buoy #1", feed_id: 'proggit' },
-                { href : "#", id : "BUOY2", name : "Buoy #2", feed_id: 'tech' },
-                { href : "#", id : "BUOY3", name : "Buoy #3", feed_id: 'webdev' },
-                { href : "#", id : "BUOY4", name : "Buoy #4", feed_id: 'drupal' }
-              ]
-            };
-
-            $("div#weather").append(template(context));
-          }
-        });
-      },
-
-      generateNavigation: function() {
-        Zepto.ajax({
-          url: './templates/navigation/main.html',
-          type: 'GET',
-          cache: true,
-          success: function (data) {
-            template = Handlebars.compile(data);
-            var context = {
-              navigation: [
-                { href : "/front", id : "history", name : "History" },
-                { href : "/front", id : "collections", name : "Collections" },
-                { href : "/front", id : "videos", name : "Videos" },
-                { href : "/front", id : "news", name : "News" }
-              ]
-            };
-
-            $("#footer-navigation").append(template(context));
-          }
-        });
+//        $("div#main-content").append(template(context));
       }
+
+//      generateBuoys: function() {
+//        Zepto.ajax({
+//          url: './templates/buoys/index.html',
+//          type: 'GET',
+//          cache: true,
+//          success: function (data) {
+//            template = Handlebars.compile(data);
+//            var context = {
+//              title: 'Weather Buoys',
+//              buoys: [
+//                { href : "#", id : "BUOY1", name : "Buoy #1", feed_id: 'proggit' },
+//                { href : "#", id : "BUOY2", name : "Buoy #2", feed_id: 'tech' },
+//                { href : "#", id : "BUOY3", name : "Buoy #3", feed_id: 'webdev' },
+//                { href : "#", id : "BUOY4", name : "Buoy #4", feed_id: 'drupal' }
+//              ]
+//            };
+//
+//            $("div#weather").append(template(context));
+//          }
+//        });
+//      },
+
+//      generateNavigation: function() {
+//        Zepto.ajax({
+//          url: './templates/navigation/main.html',
+//          type: 'GET',
+//          cache: true,
+//          success: function (data) {
+//            template = Handlebars.compile(data);
+//            var context = {
+//              navigation: [
+//                { href : "/front", id : "history", name : "History" },
+//                { href : "/front", id : "collections", name : "Collections" },
+//                { href : "/front", id : "videos", name : "Videos" },
+//                { href : "/front", id : "news", name : "News" }
+//              ]
+//            };
+//
+//            $("#footer-navigation").append(template(context));
+//          }
+//        });
+//      }
     },
 
     util: {
@@ -113,8 +120,24 @@ var Kiosk = (function($, window, document, undefined) {
           $("#drupal-news").fadeIn('slow').html(template(context));
         }
       });
+    },
+
+      page: function(url) {
+        // wipe the container
+        $("#main-content").html('');
+
+        Zepto.ajax({
+          type: "GET",
+          dataType: "html",
+          cache: false,
+          url: url,
+          success: function (result) {
+            $("#main-content").fadeIn('slow').html(result);
+          }
+        });
+      }
     }
-  }
+  
 })(typeof Zepto === 'function' ? Zepto : jQuery, this, this.document);
 
 Handlebars.registerHelper('link', function(object) {
