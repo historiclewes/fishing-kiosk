c054d1a0637815164d4e0556012d1707fd24503f
kevinquillen <kevin@inclind.com>
2013-05-08 14:41:23 -0400

    Simplify the request to Vimeo. We can either make a user request or album request.

--- javascripts/custom/vimeo.js
+++ javascripts/custom/vimeo.js
@@ -1,30 +1,33 @@
-// Define VimeoAjaxRequest
-var VimeoAjaxRequest = (function () {
-  var fetchVideoData = function(vimeo_id, callback) {
+// Define VimeoRequest
+var VimeoRequest = (function () {
+  var getData = function(type, id, callback) {
     Zepto.ajax(
         {
-          url: VimeoAjaxRequest.vimeoVideoAPIUrl(vimeo_id),
+          url: vimeoAPIRequestURL(type, id),
           dataType: 'jsonp',
           type: 'GET',
           cache: false,
           success: function (result) {
-            callback(result[0]);
+            callback(result);
           }
         }
     );
   }
 
-  return {
-    fetchVideoData : fetchVideoData,
-
-    vimeoVideoAPIUrl: function(vimeo_id) {
-      return 'http://vimeo.com/api/v2/video/' + vimeo_id + '.json';
-    },
-
-    getThumbnail: function(vimeo_id) {
-      VimeoAjaxRequest.fetchVideoData(vimeo_id, function(response) {
-        return response.thumbnail_medium;
-      });
+  var vimeoAPIRequestURL = function(type, id) {
+    switch (type) {
+      case 'user':
+        return 'http://vimeo.com/api/v2/' + id + '/videos.json';
+        break;
+      case 'album':
+        return 'http://vimeo.com/api/v2/album/' + id + '/videos.json';
+        break;
+      default:
+        break;
     }
+  }
+
+  return {
+    getData : getData
   }
 })();
