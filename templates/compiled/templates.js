c2272c3b5c972ffd1365294890a236eb3f730d43
kevinquillen <kevin@inclind.com>
2013-05-10 15:07:32 -0400

    Update compiled templates

--- templates/compiled/templates.js
+++ templates/compiled/templates.js
@@ -3,18 +3,58 @@
 templates['bay.hbs'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Handlebars.helpers; data = data || {};
+  var buffer = "", stack1, stack2, options, functionType="function", escapeExpression=this.escapeExpression, self=this, helperMissing=helpers.helperMissing;
+
+function program1(depth0,data) {
   
+  var buffer = "", stack1;
+  buffer += " <a href=\"#\" id=\"";
+  if (stack1 = helpers.id) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.id; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\" class=\"button radius\" onclick=\"Kiosk.getWeatherData('";
+  if (stack1 = helpers.id) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.id; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "'); return false;\">";
+  if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</a> ";
+  return buffer;
+  }
 
-
-  return "<div class=\"row\">\n  <div class=\"large-5 columns text-center\" id=\"bay-map\">\n    <h2 class=\"block-title\">Current<br/> Shipping Traffic</h2>\n\n    <div class=\"map\">\n      <iframe name=\"marinetraffic\" id=\"marinetraffic\" width=\"425\" height=\"415\" scrolling=\"no\" src=\"http://www.marinetraffic.com/ais/embed.aspx?zoom=10&remember=false&shownames=true&notation=false&border=0&maptype=1&latitude=38.7744&longitude=75.1397\"></iframe>\n    </div>\n    <div class=\"large-12 columns\"><span class=\"text-center\">Touch a ship to view more information</span>\n    </div>\n  </div>\n\n\n  <div class=\"large-5 columns\" id=\"current-conditions-block\">\n    <h2 class=\"block-title text-center\">Current Conditions</h2>\n\n    <div class=\"large-12 columns\" id=\"current-conditions\">\n      <div class=\"large-4 columns\" id=\"temperature\">\n        <h4 class=\"text-center\">Temperature</h4>\n        <img src=\"http://placehold.it/125x125\"/>\n\n        <div class=\"row\">\n          <div class=\"large-12 columns\">\n            <h4 class=\"left\">Air:</h4>\n            <span class=\"red-span\">75ºF</span>\n          </div>\n          <div class=\"large-12 columns\">\n            <h4 class=\"left\">Water:</h4>\n            <span class=\"red-span\">65ºF</span>\n          </div>\n        </div>\n      </div>\n      <div class=\"large-4 columns \" id=\"pressure\">\n        <h4 class=\"text-center\">Air Pressure</h4>\n        <img src=\"http://placehold.it/125x125\"/>\n\n        <div class=\"row\">\n\n          <div class=\"large-12 columns\">\n            <span class=\"red-span text-center\">29.56 inches</span>\n          </div>\n          <div class=\"large-12 columns\">\n            <h4 class=\"text-center\">Rising</h4>\n          </div>\n        </div>\n      </div>\n      <div class=\"large-4 columns\" id=\"windspeed\">\n        <h4 class=\"text-center\">Wind Speed</h4>\n        <img src=\"http://placehold.it/125x125\"/>\n\n        <div class=\"row\">\n\n          <div class=\"large-12 columns\">\n            <span class=\"red-span\">10 Knots</span>\n            <h4 class=\"left\">SSW</h4>\n          </div>\n          <div class=\"large-12 columns\">\n            <h4 class=\"left\">Gusts:</h4>\n            <span class=\"red-span\">10 Kt</span>\n          </div>\n        </div>\n      </div>\n    </div>\n\n\n  </div>\n  <div class=\"large-2 columns\" id=\"buoy-stations\">\n    <h2 class=\"block-title text-center\">Buoy Stations</h2>\n\n    <div class=\"navigation\">\n      <li><a class=\"button radius\" href=\"#\">Station SJSN 4</a></li>\n      <li><a class=\"button radius\" href=\"#\">Station SJSN 4</a></li>\n      <li><a class=\"button radius\" href=\"#\">Station SJSN 4</a></li>\n      <li><a class=\"button radius\" href=\"#\">Station SJSN 4</a></li>\n    </div>\n  </div>\n  <div class=\"large-6 columns\" id=\"waves-tides\">\n    <div class=\"row waves-tides-header\">\n      <div class=\"waves-tides-header\"></div>\n      <div class=\"large-7 columns  block-radius\">\n        <h2 class=\"block-title text-center\">Waves</h2>\n\n        <div class=\"row\">\n\n          <div class=\"large-6 columns\">\n            <h4 class=\"left\">Height:</h4>\n            <span class=\"red-span\">2 feet</span>\n          </div>\n\n          <div class=\"large-6 columns\">\n            <h4 class=\"left\">Length:</h4>\n            <span class=\"red-span\">100 m</span>\n          </div>\n\n          <div class=\"large-6 columns\">\n            <h4 class=\"left\">Period:</h4>\n            <span class=\"red-span\">3.2 sec</span>\n          </div>\n\n          <div class=\"large-6 columns\">\n            <h4 class=\"left\">Direction:</h4>\n            <span class=\"red-span\">NW</span>\n          </div>\n\n        </div>\n      </div>\n\n      <div class=\"large-4 columns block-radius\">\n        <h2 class=\"block-title text-center\">Tides</h2>\n\n        <div class=\"row\">\n\n          <div class=\"large-12 columns\">\n            <h4 class=\"left\">Low:</h4>\n            <span class=\"red-span\">1:34 AM</span>\n          </div>\n\n          <div class=\"large-12 columns\">\n            <h4 class=\"left\">High:</h4>\n            <span class=\"red-span\">12:53 PM</span>\n          </div>\n        </div>\n\n      </div>\n    </div>\n  </div>\n</div>\n";
+  buffer += "<div class=\"row\">\n  <div class=\"large-5 columns text-center\" id=\"bay-map\">\n    <h2 class=\"block-title\">Current<br/> Shipping Traffic</h2>\n\n    <div class=\"map\">\n      <iframe name=\"marinetraffic\" id=\"marinetraffic\" width=\"425\" height=\"415\" scrolling=\"no\" src=\"http://www.marinetraffic.com/ais/embed.aspx?zoom=10&remember=false&shownames=true&notation=false&border=0&maptype=1&latitude=38.7744&longitude=75.1397\"></iframe>\n    </div>\n    <div class=\"large-12 columns\"><span class=\"text-center\">Touch a ship to view more information</span>\n    </div>\n  </div>\n\n  <div class=\"large-5 columns\" id=\"current-conditions-block\"></div>\n\n  <div class=\"large-2 columns\" id=\"buoy-stations\">\n    <h2 class=\"block-title text-center\">Buoy Stations</h2>\n\n    <div class=\"navigation\">\n      ";
+  options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data};
+  stack2 = ((stack1 = helpers.list),stack1 ? stack1.call(depth0, depth0.buoys, options) : helperMissing.call(depth0, "list", depth0.buoys, options));
+  if(stack2 || stack2 === 0) { buffer += stack2; }
+  buffer += "\n    </div>\n  </div>\n\n  <div class=\"large-6 columns\" id=\"waves-tides\"></div>\n</div>\n";
+  return buffer;
   });
-templates['buoys.hbs'] = template(function (Handlebars,depth0,helpers,partials,data) {
+templates['buoy.hbs'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Handlebars.helpers; data = data || {};
-  
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
 
 
-  return "<div class=\"row\">\n  <h2>Buoys</h2>\n</div>";
+  buffer += "<div class=\"row waves-tides-header\">\n  <div class=\"waves-tides-header\"></div>\n  <div class=\"large-7 columns block-radius\">\n    <h2 class=\"block-title text-center\">Waves</h2>\n\n    <div id=\"wave-data\" class=\"row\">\n      <div class=\"large-6 columns\">\n        <h4 class=\"left\">Height:</h4>\n        <span class=\"red-span\">";
+  if (stack1 = helpers.wave_height) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.wave_height; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</span>\n      </div>\n\n      <div class=\"large-6 columns\">\n        <h4 class=\"left\">Length:</h4>\n        <span class=\"red-span\">";
+  if (stack1 = helpers.wave_length) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.wave_length; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</span>\n      </div>\n\n      <div class=\"large-6 columns\">\n        <h4 class=\"left\">Period:</h4>\n        <span class=\"red-span\">";
+  if (stack1 = helpers.wave_period) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.wave_period; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</span>\n      </div>\n\n      <div class=\"large-6 columns\">\n        <h4 class=\"left\">Direction:</h4>\n        <span class=\"red-span\">";
+  if (stack1 = helpers.wave_direction) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.wave_direction; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</span>\n      </div>\n\n    </div>\n  </div>\n\n  <div class=\"large-4 columns block-radius\">\n    <h2 class=\"block-title text-center\">Tides</h2>\n\n    <div id=\"tide-data\" class=\"row\">\n      <div class=\"large-12 columns\">\n        <h4 class=\"left\">Low:</h4>\n        <span class=\"red-span\">1:34 AM</span>\n      </div>\n\n      <div class=\"large-12 columns\">\n        <h4 class=\"left\">High:</h4>\n        <span class=\"red-span\">12:53 PM</span>\n      </div>\n    </div>\n  </div>\n</div>";
+  return buffer;
   });
 templates['collection.hbs'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
@@ -72,13 +112,50 @@ function program2(depth0,data) {
   buffer += "\n    </div>\n    <div class=\"collection-teaser-bottom\"></div>\n  </div>\n</div>";
   return buffer;
   });
+templates['conditions.hbs'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
+
+
+  buffer += "<h2 class=\"block-title text-center\">Current Conditions</h2>\n\n<div class=\"large-12 columns\" id=\"current-conditions\">\n  <div class=\"large-4 columns\" id=\"temperature\">\n    <h4 class=\"text-center\">Temperature</h4>\n    <img src=\"http://placehold.it/125x125\"/>\n\n    <div class=\"row\">\n      <div class=\"large-12 columns\">\n        <h4 class=\"left\">Air:</h4>\n        <span class=\"red-span\">";
+  if (stack1 = helpers.air_temperature) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.air_temperature; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</span>\n      </div>\n      <div class=\"large-12 columns\">\n        <h4 class=\"left\">Water:</h4>\n        <span class=\"red-span\">";
+  if (stack1 = helpers.water_temperature) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.water_temperature; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</span>\n      </div>\n    </div>\n  </div>\n  <div class=\"large-4 columns \" id=\"pressure\">\n    <h4 class=\"text-center\">Air Pressure</h4>\n    <img src=\"http://placehold.it/125x125\"/>\n\n    <div class=\"row\">\n      <div class=\"large-12 columns\">\n        <span class=\"red-span text-center\">";
+  if (stack1 = helpers.air_pressure) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.air_pressure; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</span>\n      </div>\n      <div class=\"large-12 columns\">\n        <h4 class=\"text-center\">";
+  if (stack1 = helpers.air_pressure_change) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.air_pressure_change; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</h4>\n      </div>\n    </div>\n  </div>\n  <div class=\"large-4 columns\" id=\"windspeed\">\n    <h4 class=\"text-center\">Wind Speed</h4>\n    <img src=\"http://placehold.it/125x125\"/>\n\n    <div class=\"row\">\n      <div class=\"large-12 columns\">\n        <span class=\"red-span\">";
+  if (stack1 = helpers.wind_speed) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.wind_speed; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</span>\n        <h4 class=\"left\">";
+  if (stack1 = helpers.wind_direction) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.wind_direction; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</h4>\n      </div>\n      <div class=\"large-12 columns\">\n        <h4 class=\"left\">Gusts:</h4>\n        <span class=\"red-span\">";
+  if (stack1 = helpers.wind_gusts) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.wind_gusts; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</span>\n      </div>\n    </div>\n  </div>\n</div>";
+  return buffer;
+  });
 templates['footer.hbs'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Handlebars.helpers; data = data || {};
   
 
 
-  return "<div class=\"row\">\n  <div class=\"large-10 columns\">\n    <nav id=\"footer-navigation\">\n      <ul class=\"inline-list\">\n        <li class=\"home\"><a id=\"home\" href=\"#\" onclick=\"Kiosk.getPage('home'); return false;\">Home</a></li>\n        <li class=\"the-bay\"><a id=\"the-bay\" href=\"#\" onclick=\"Kiosk.getPage('bay'); return false;\">The Bay</a></li>\n        <li class=\"history\"><a href=\"#\" onclick=\"Kiosk.getNode('node', 323); return false;\">History</a></li>\n        <li class=\"collection\"><a href=\"#\" onclick=\"Kiosk.getCollections(); return false;\">Collection</a></li>\n        <li class=\"videos\"><a href=\"#\" onclick=\"Kiosk.getVideos(); return false;\">Videos</a></li>\n        <li class=\"cannonball-house\"><a href=\"#\" onclick=\"Kiosk.getNode('node', 368); return false;\">Cannonball House</a></li>\n      </ul>\n    </nav>\n  </div>\n  <div class=\"large-2 columns text-center\" id=\"newsletter\">\n        <span>\n          Stay up to date with LHS Newsletter!\n        </span><br/><br/>\n    <a href=\"#\" data-reveal-id=\"newsletter-signup\">\n      Join today\n    </a>\n  </div>\n</div>";
+  return "<div class=\"row\">\n  <div class=\"large-10 columns\">\n    <nav id=\"footer-navigation\">\n      <ul class=\"inline-list\">\n        <li class=\"home\"><a id=\"home\" href=\"#\" onclick=\"Kiosk.getPage('home'); return false;\">Home</a></li>\n        <li class=\"the-bay\"><a id=\"the-bay\" href=\"#\" onclick=\"Kiosk.getBay(); return false;\">The Bay</a></li>\n        <li class=\"history\"><a href=\"#\" onclick=\"Kiosk.getNode('node', 323); return false;\">History</a></li>\n        <li class=\"collection\"><a href=\"#\" onclick=\"Kiosk.getCollections(); return false;\">Collection</a></li>\n        <li class=\"videos\"><a href=\"#\" onclick=\"Kiosk.getVideos(); return false;\">Videos</a></li>\n        <li class=\"cannonball-house\"><a href=\"#\" onclick=\"Kiosk.getNode('node', 368); return false;\">Cannonball House</a></li>\n      </ul>\n    </nav>\n  </div>\n  <div class=\"large-2 columns text-center\" id=\"newsletter\">\n        <span>\n          Stay up to date with LHS Newsletter!\n        </span><br/><br/>\n    <a href=\"#\" data-reveal-id=\"newsletter-signup\">\n      Join today\n    </a>\n  </div>\n</div>";
   });
 templates['header.hbs'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
