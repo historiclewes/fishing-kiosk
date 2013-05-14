8e2bacc401664a9d8fe04b3080eb895fdc176379
nickBumgarner <nick@inclind.com>
2013-05-14 11:01:03 -0400

    Updating fonts styles and javascript

--- javascripts/custom/navigation.js
+++ javascripts/custom/navigation.js
@@ -1,18 +1,7 @@
 $(document).ready(function () {
   var footer = $('#footer-navigation');  // cache footer to a variable for performance
-
   footer.delegate('li.inactive', 'click', function () {
     footer.find('.active').toggleClass('active inactive');
     $(this).toggleClass('active inactive');
   });
-
-  var buoys = $('#buoy-stations .navigation');  // cache footer to a variable for performance
-
-  buoys.delegate('li', 'click', function () {
-    buoys.find("li").addClass("inactive");
-    buoys.find('.active').toggleClass('active inactive');
-    $(this).toggleClass('active inactive');
-  });
-
-
 });
