4aaa9fea935ebfc84b12e5cd71f4e257a54fb4ca
nickBumgarner <nick@inclind.com>
2013-05-14 10:47:15 -0400

    Updloading style changes to the site

--- javascripts/custom/navigation.js
+++ javascripts/custom/navigation.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
-  $("li.inactive").click(function () {
+  var footer = $('#footer-navigation');  // cache footer to a variable for performance
 
-    alert("Hello!");
-
-    $(this).addClass("active");
+  footer.delegate('li.inactive', 'click', function () {
+    footer.find('.active').toggleClass('active inactive');
+    $(this).toggleClass('active inactive');
   });
+
+  var buoys = $('#buoy-stations .navigation');  // cache footer to a variable for performance
+
+  buoys.delegate('li', 'click', function () {
+    buoys.find("li").addClass("inactive");
+    buoys.find('.active').toggleClass('active inactive');
+    $(this).toggleClass('active inactive');
+  });
+
+
 });
