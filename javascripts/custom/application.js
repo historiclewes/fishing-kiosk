84984c7b5a473977f412f2ba332ea976af210423
kevinquillen <kevin@inclind.com>
2013-05-14 23:05:33 -0400

    Refactor the code to prep for launch of the kiosk. All of the assembly of data now happens in the methods being called rather than split it up. This application.js solely handles actions and requests and returns data to be rendered with Handlebars.

--- javascripts/custom/application.js
+++ javascripts/custom/application.js
@@ -4,28 +4,6 @@ var Kiosk = (function($, window, document, undefined) {
     Kiosk.go();
   });
 
-  // Application variables
-  var contentServer = 'http://www.historiclewes.org/';
-
-  // define our services endpoints back to the parent application
-  var services = {
-    node: 'kiosk/views/kiosk_nodes?display_id=block_1',
-    collections: 'kiosk/views/kiosk_nodes?display_id=block_2',
-    slideshow: 'kiosk/views/kiosk_nodes?display_id=block_3',
-    search: 'kiosk/views/kiosk_nodes?display_id=block_4'
-  }
-
-  var rssFeeds = {
-    cnn_us: 'http://rss.cnn.com/rss/cnn_us.rss',
-  }
-
-  var vimeo = {
-    'type': 'user',
-    'id': 'user1949213'
-  };
-
-  // End application variables
-
   return {
     go: function() {
       var i, j = this.init;
@@ -58,22 +36,6 @@ var Kiosk = (function($, window, document, undefined) {
       Kiosk.util.updateScreen('#main-content', Template());
     },
 
-    // generic function to call and load local HTML files with optional Handlebars components
-    getSlideshow: function(template) {
-      var template = Handlebars.getTemplate(template);
-
-      DrupalRequest.fetchView('slideshow', function(response) {
-        var context = { items: [] };
-
-        $.each(response, function(key, value) {
-          context.items.push({ 'src' : value.picture });
-        });
-
-        Kiosk.util.updateScreen('#slideshow', template(context));
-        Foundation.libs.orbit.init();
-      });
-    },
-
     // render the bay page and load the weather data for the first buoy
     getBay: function() {
       var template = Handlebars.getTemplate('bay');
@@ -95,17 +57,7 @@ var Kiosk = (function($, window, document, undefined) {
       var template = Handlebars.getTemplate(template);
 
       DrupalRequest.fetchNode(node_id, function(response) {
-        var context = {
-          title: response[0].title,
-          body: response[0].body
-        }
-
-        // if a kiosk description exists, use that instead of the existing node body
-        if (response[0].kiosk_body && response[0].kiosk_body.length) {
-          context.body = response[0].kiosk_body;
-        }
-
-        Kiosk.util.updateScreen('#main-content', template(context));
+        Kiosk.util.updateScreen('#main-content', template(response));
       });
     },
 
@@ -113,28 +65,27 @@ var Kiosk = (function($, window, document, undefined) {
     getCollections: function() {
       var template = Handlebars.getTemplate('collections');
 
-      DrupalRequest.fetchView('collections', function(response) {
-        var context = { items: [] }
+      DrupalRequest.fetchCollectionsView('collections', function(response) {
+        Kiosk.util.updateScreen('#main-content', template(response));
+      });
+    },
 
-        $.each(response, function(key, value) {
-          context.items.push({ 'title' : value.title, 'teaser': value.teaser, 'nid': value.nid });
-        });
+    // generic function to call and load local HTML files with optional Handlebars components
+    getSlideshow: function(template) {
+      var template = Handlebars.getTemplate(template);
 
-        Kiosk.util.updateScreen('#main-content', template(context));
+      DrupalRequest.fetchSlideshowView('slideshow', function(response) {
+        Kiosk.util.updateScreen('#slideshow', template(response));
+        Foundation.libs.orbit.init();
       });
     },
 
     // fetch Videos
     getVideos: function() {
       var template = Handlebars.getTemplate('videos');
-      var context = { items: [] }
 
-      VimeoRequest.getData(vimeo.type, vimeo.id, function(response) {
-        $.each(response, function(key, value) {
-          context.items.push({ 'title' : value.title, 'vimeo_id': value.id, 'thumbnail_small': value.thumbnail_small, 'thumbnail_medium': value.thumbnail_medium, 'thumbnail_large': value.thumbnail_large});
-        });
-
-        Kiosk.util.updateScreen('#main-content', template(context));
+      VimeoRequest.getData(Kiosk.variables.vimeo.type, Kiosk.variables.vimeo.id, function(response) {
+        Kiosk.util.updateScreen('#main-content', template(response));
       });
     },
 
@@ -150,14 +101,9 @@ var Kiosk = (function($, window, document, undefined) {
     getSearchResults: function() {
       var template = Handlebars.getTemplate('search');
       var keywords = $('#kiosk-search').attr('value');
-      var context = { items: [], keywords: keywords }
 
       DrupalRequest.doSearch('search', keywords, function(response) {
-        $.each(response, function(key, value) {
-          context.items.push({ 'title' : value.title, 'nid': value.nid, 'teaser': value.teaser });
-        });
-
-        Kiosk.util.updateScreen('#main-content', template(context));
+        Kiosk.util.updateScreen('#main-content', template(response));
       });
     },
 
@@ -177,22 +123,8 @@ var Kiosk = (function($, window, document, undefined) {
       var conditionsTemplate = Handlebars.getTemplate('conditions');
 
       BuoyRequest.getData(buoy_id, function(response) {
-        var context = {
-          wave_height: response[0].WVHT,
-          wave_length: response[0].wavelength,
-          wave_period: response[0].DPD,
-          wave_direction: response[0].MWD,
-          air_pressure: response[0].PRES,
-          air_pressure_change: response[0].PTDY,
-          air_temperature: response[0].ATMP,
-          water_temperature: response[0].WTMP,
-          wind_speed: response[0].WSPD,
-          wind_direction: response[0].WDIR,
-          wind_gusts: response[0].GST,
-        };
-
-        Kiosk.util.updateScreen('#current-conditions-block', conditionsTemplate(context));
-        Kiosk.util.updateScreen('#waves-tides', buoyTemplate(context));
+        Kiosk.util.updateScreen('#current-conditions-block', conditionsTemplate(response));
+        Kiosk.util.updateScreen('#waves-tides', buoyTemplate(response));
 
         var options = {
           min: 0,
@@ -210,7 +142,7 @@ var Kiosk = (function($, window, document, undefined) {
           }
         }
 
-        Kiosk.drawGauge(response[0].ATMP, 'air_temperature_gauge', options);
+        Kiosk.drawGauge(response.air_temperature, 'air_temperature_gauge', options);
 
         var options = {
           min: 10,
@@ -226,7 +158,7 @@ var Kiosk = (function($, window, document, undefined) {
           }
         }
 
-        Kiosk.drawGauge(response[0].PRES, 'air_pressure_gauge', options);
+        Kiosk.drawGauge(response.air_pressure, 'air_pressure_gauge', options);
 
         var options = {
           min: 0,
@@ -244,7 +176,7 @@ var Kiosk = (function($, window, document, undefined) {
           }
         }
 
-        Kiosk.drawGauge(response[0].WSPD, 'wind_speed_gauge', options);
+        Kiosk.drawGauge(response.wind_speed, 'wind_speed_gauge', options);
       });
     },
 
@@ -252,53 +184,43 @@ var Kiosk = (function($, window, document, undefined) {
       var email_address = $('#newsletter-signup input#newsletter-email-address').attr('value');
 
       DrupalRequest.newsletterSignup(email_address, function(response) {
-        $('#newsletter-signup').foundation('reveal', 'close');
+        $('#newsletter-signup').html('<div class="small-12 columns"><p>Thank you! A confirmation has been sent to ' + email_address + '. Please follow the instructions in that email to complete the signup for our newsletter.</p></div>');
+
+        setTimeout(function() {
+          $('#newsletter-signup').foundation('reveal', 'close');
+        }, 5000);
       });
     },
 
     util: {
       actionUrl: function(feed_id) {
-        return contentServer + services[feed_id];
+        return Kiosk.variables.contentServer + Kiosk.variables.services[feed_id];
       },
 
       updateScreen: function(selector, content) {
         $(selector).fadeIn('slow').html(content);
       }
+    },
+
+    // Application variables
+    variables: {
+      contentServer: 'http://www.historiclewes.org/',
+
+      services: {
+        node: 'kiosk/views/kiosk_nodes?display_id=block_1',
+        collections: 'kiosk/views/kiosk_nodes?display_id=block_2',
+        slideshow: 'kiosk/views/kiosk_nodes?display_id=block_3',
+        search: 'kiosk/views/kiosk_nodes?display_id=block_4'
+      },
+
+      rssFeeds: {
+        cnn_us: 'http://rss.cnn.com/rss/cnn_us.rss'
+      },
+
+      vimeo: {
+        'type': 'user',
+        'id': 'user1949213'
+      }
     }
   }
 })(typeof Zepto === 'function' ? Zepto : jQuery, this, this.document);
-
-// Register a 'link' function through Handlebars as a formatter helper.
-Handlebars.registerHelper('link', function(object) {
-  return new Handlebars.SafeString(
-    "<a href='" + object.url + "'>" + object.text + "</a>"
-  );
-});
-
-Handlebars.registerHelper('list', function(items, options) {
-  var output = '';
-
-  for(var i=0, l=items.length; i<l; i++) {
-    output = output + "<li>" + options.fn(items[i]) + "</li>";
-  }
-
-  return output;
-});
-
-Handlebars.getTemplate = function(name) {
-  if (Handlebars.templates === undefined || Handlebars.templates[name] === undefined) {
-    $.ajax({
-      type: 'GET',
-      url : 'templates/handlebars/' + name + '.hbs',
-      cache: false,
-      success : function(data) {
-        if (Handlebars.templates === undefined) {
-          Handlebars.templates = {};
-        }
-        Handlebars.templates[name] = Handlebars.compile(data);
-      },
-      async : false
-    });
-  }
-  return Handlebars.templates[name];
-};
